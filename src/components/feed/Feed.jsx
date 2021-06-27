7ae9a7cc74fc2353710bb523b70f6e40ed3e6ecd
Natty <daratchanan.p@gmail.com>
2021-06-27 17:29:01 +0700

    sidebar feed

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,9 +1,13 @@
-import "./feed.css"
+import "./feed.css";
+import Share from "../share/Share";
+
 
 export default function Feed() {
    return (
-      <div>
-         feed
+      <div className="feed">
+         <div className="feedWrapper">
+            <Share />
+         </div>
       </div>
    )
 }
